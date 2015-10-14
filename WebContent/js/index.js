0d26df4b6855b9f0f11791108f5ef44d9cd857e5
Ray <rui.song.2013@sis.smu.edu.sg>
2015-10-14 15:49:00 +0800

    implement login and register function
    can refer to index.js for Java Script (Jquery) reference on how to
    utilise the localStorage.

--- WebContent/js/index.js
+++ WebContent/js/index.js
@@ -1,72 +1,76 @@
 $(document).ready(function() {
-	$("#registerForm").hide();
+	$("#registerFormContainer").hide();
 });
 
-
-$("#login-button").click(function(event){
-	 event.preventDefault();
-	 
-	 $("#headText").html("Welcome");
-	 $('form').fadeOut(200);
-	 $('.wrapper').addClass('form-success');
-	 
-	 setTimeout(function () {
-		 window.location.href = "home.html"; //will redirect to your blog page (an ex: blog.html)
-     }, 2000); //will call the function after 2 secs.
+$("#login-button").click(function(event) {
+	event.preventDefault();
+	
+	var username = $("#loginUsername").val();
+	var password = $("#loginPassword").val();
+	
+	var verifyUsername = localStorage.getItem('username');
+	var verifyPassword = localStorage.getItem('password');
+	
+	if(username == verifyUsername){
+		$("#headText").html("Welcome " + username);
+		$('form').fadeOut(200);
+		$('.wrapper').addClass('form-success');
+		
+		localStorage.setItem('currentUser', username);
+		
+		setTimeout(function() {
+			window.location.href = "home.html"; // will redirect to your blog page
+												// (an ex: blog.html)
+		}, 2000); // will call the function after 2 secs.
+	}else{
+		$("#loginErrorMessage").html("The user does not exist or the password is incorrect.");
+	}
+	
 });
 
+$("#register-button").click(function(event) {
+	event.preventDefault();
+	
+	$("#loginErrorMessage").html('');
+	$('#loginFormContainer').hide();
+	$("#registerFormContainer").fadeIn();
 
-$("#register-button").click(function(event){
-	 event.preventDefault();
-	 
-	 $('#loginForm').hide();
-	 $("#registerForm").fadeIn();
-
-	 
-//	 setTimeout(function () {
-//		 window.location.href = "home.html"; //will redirect to your blog page (an ex: blog.html)
-//    }, 2000); //will call the function after 2 secs.
+	// setTimeout(function () {
+	// window.location.href = "home.html"; //will redirect to your blog page (an
+	// ex: blog.html)
+	// }, 2000); //will call the function after 2 secs.
 });
 
-$("#cancel-button").click(function(event){
-	 event.preventDefault();
-	 
-	 $("#registerForm").hide();
-	 $('#loginForm').fadeIn();
+$("#register-in").click(function(event) {
+	event.preventDefault();
+	
+	$("#loginErrorMessage").html('');
+	var username = $("#regUsername").val();
+	var password = $("#regPassword").val();
+	var password2 = $("#confirmPassword").val();
+	
+	if(password == password2){
+		localStorage.setItem('username', username);
+		localStorage.setItem('password', password);
+		
+		$("#registerFormContainer").hide();
+		$('#loginFormContainer').fadeIn();
+	}else{
+		$("#registerErrorMessage").html("Password does not match, try again.");
+	}
+	
+	
+
+	// setTimeout(function () {
+	// window.location.href = "home.html"; //will redirect to your blog page (an
+	// ex: blog.html)
+	// }, 2000); //will call the function after 2 secs.
 });
 
-
-function login(){
-	//console.log("enter");
-	var email = $("#email").val();
-	var password = $("#password").val();
-	var input = {};
-	input.email = email;
-	input.password = password;
-	var inputStr = JSON.stringify(input);
-	inputStr = encodeURIComponent(inputStr);
-	$.ajax({
-		url:'/Payroll/admin/GetApplicationByEmailServlet?input='+inputStr,
-		method:'POST',
-		dataType: 'json',
-		error: function(err){
-			console.log(err);
-			$("#message").html("system failed to login in");
-		},
-		success: function(data){
-			console.log(data);
-			var status = data.status;
-			var message = data.message;
-			if(status== 1){
-				var applicationId = message.applicationId;
-				localStorage.setItem('applicationId', applicationId);
-				var app = localStorage.getItem('application');
-				var appChecking = JSON.parse(app) ;
-				window.location = "index.html";
-			}else{
-				$("#message").html("Invalid Eamil/Password");
-				console.log(message);
-			}
-		}
-	});
-}
+$("#cancel-button").click(function(event) {
+	event.preventDefault();
+	
+	$("#loginErrorMessage").html('');
+	$("#registerFormContainer").hide();
+	$('#loginFormContainer').fadeIn();
+});
