eca7297e51aff5d7701b1df9a30ac2fccc38c849
tweetweetarnold <arnold.lee.wt@gmail.com>
2015-10-15 23:51:55 +0800

    Include new data.js file

---
+++ WebContent/js/data.js
@@ -0,0 +1,82 @@
+/**
+ * Data
+ */
+
+// Nurse
+var nurse = {
+	name : "Juliet",
+	notification : false
+};
+
+// Residents
+var residentList = [ {
+	name : "Alex",
+	bed : 101,
+	pastAppt : [ '11 Sep, Fri', '9 Oct, Fri' ],
+	appt : [ '6 Nov, Fri', '11 Dec, Fri' ],
+	contact : [ {
+		name : "Harry Armstrong",
+		relation : "Father",
+		contact : 98765425
+	}, {
+		name : "Angelababy",
+		relation : "Mother",
+		contact : 56489565
+	} ]
+}, {
+	name : "John",
+	bed : 102,
+	pastAppt : [ '11 Sep, Fri', '9 Oct, Fri' ],
+	appt : [ '6 Nov, Fri', '11 Dec, Fri' ],
+	contact : [ {
+		name : "Harry Armstrong",
+		relation : "Father",
+		contact : 98765425
+	}, {
+		name : "Angelababy",
+		relation : "Mother",
+		contact : 56489565
+	} ]
+}, {
+	name : "Charlie",
+	bed : 103,
+	pastAppt : [ '11 Sep, Fri', '9 Oct, Fri' ],
+	appt : [ '6 Nov, Fri', '11 Dec, Fri' ],
+	contact : [ {
+		name : "Harry Armstrong",
+		relation : "Father",
+		contact : 98765425
+	}, {
+		name : "Angelababy",
+		relation : "Mother",
+		contact : 56489565
+	} ]
+}, {
+	name : "Derrick",
+	bed : 104,
+	pastAppt : [ '11 Sep, Fri', '9 Oct, Fri' ],
+	appt : [ '6 Nov, Fri', '11 Dec, Fri' ],
+	contact : [ {
+		name : "Harry Armstrong",
+		relation : "Father",
+		contact : 98765425
+	}, {
+		name : "Angelababy",
+		relation : "Mother",
+		contact : 56489565
+	} ]
+}, {
+	name : "Jack",
+	bed : 105,
+	pastAppt : [ '11 Sep, Fri', '9 Oct, Fri' ],
+	appt : [ '6 Nov, Fri', '11 Dec, Fri' ],
+	contact : [ {
+		name : "Harry Armstrong",
+		relation : "Father",
+		contact : 98765425
+	}, {
+		name : "Angelababy",
+		relation : "Mother",
+		contact : 56489565
+	} ]
+} ];
