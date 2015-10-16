db6a92890ecb020f13c5a63208c329134c41843b
tweetweetarnold <arnold.lee.wt@gmail.com>
2015-10-17 02:25:41 +0800

    Update home to include newsfeed from residentfeed

--- WebContent/js/data.js
+++ WebContent/js/data.js
@@ -5,7 +5,7 @@
 $(document).ready(function() {
 	var nurseStr = JSON.stringify(nurse);
 	localStorage.setItem('Nurse', nurseStr);
-	
+
 	var residentListStr = JSON.stringify(residentList);
 	localStorage.setItem('residentList', residentListStr);
 });
@@ -22,13 +22,13 @@ var residentList = [ {
 	bed : 101,
 	image : '//lorempixel.com/150/150/people/1/',
 	news : [ {
-		date : "11 Oct, Sun, 1830hrs",
+		date : "11 Oct, Sun, 1030hrs",
 		icon : "./img/NewsFeedIcon.png",
-		description : "Alex fell down in the toilet at about 5pm just now"
+		description : "Jack fell down in the toilet"
 	}, {
-		date : "11 Oct, Sun, 1500hrs",
+		date : "11 Oct, Sun, 0300hrs",
 		icon : "./img/NewsFeedIcon.png",
-		description : "Jack choked on fishballs during lunch"
+		description : "Jack suffered a seizure"
 	} ],
 	pastAppt : [ {
 		date : "11 Sep",
@@ -50,11 +50,11 @@ var residentList = [ {
 	} ],
 	contact : [ {
 		name : "Harry Armstrong",
-		relation : "Father",
+		relation : "Son",
 		contact : 98765425
 	}, {
 		name : "Angelababy",
-		relation : "Mother",
+		relation : "Wife",
 		contact : 56489565
 	} ],
 	allergies : [ {
@@ -64,7 +64,7 @@ var residentList = [ {
 	}, {
 		name : "Egg"
 	} ],
-        diagnoses : [ {
+	diagnoses : [ {
 		name : "High Blood Pressure"
 	}, {
 		name : "Diabetes"
@@ -72,23 +72,27 @@ var residentList = [ {
 		name : "Kidney"
 	} ],
 	medicine : [ {
-                name: "Ibuprofen",
+		name : "Ibuprofen",
 		dosage : "400mg",
 		frequency : "Twice a day",
 		beganon : "17 May 2015",
-                sideEffects : ""
-	},
-        {
-                name: "Paracetamol",
+		sideEffects : ""
+	}, {
+		name : "Paracetamol",
 		dosage : "200mg",
 		frequency : "Once a day",
 		beganon : "1 Nov 2015",
-                sideEffects : ""
-	}]
+		sideEffects : ""
+	} ]
 }, {
 	name : "John",
 	bed : 102,
 	image : '//lorempixel.com/150/150/people/2/',
+	news : [ {
+		date : "10 Oct, Sat, 1500hrs",
+		icon : "./img/ApptIcon.png",
+		description : "John forgot to go for medical appointment"
+	} ],
 	pastAppt : [ {
 		date : "11 Sep",
 		time : 1200,
@@ -109,17 +113,26 @@ var residentList = [ {
 	} ],
 	contact : [ {
 		name : "Harry Armstrong",
-		relation : "Father",
+		relation : "Son",
 		contact : 98765425
 	}, {
 		name : "Angelababy",
-		relation : "Mother",
+		relation : "Wife",
 		contact : 56489565
 	} ]
 }, {
 	name : "Charlie",
 	bed : 103,
 	image : '//lorempixel.com/150/150/people/3/',
+	news : [ {
+		date : "11 Oct, Sun, 1130hrs",
+		icon : "./img/NewsFeedIcon.png",
+		description : "Charlie got scalded by hot porridge during lunch"
+	}, {
+		date : "10 Oct, Sat, 1500hrs",
+		icon : "./img/ApptIcon.png",
+		description : "Charlie's medical appointment is updated"
+	} ],
 	pastAppt : [ {
 		date : "11 Sep",
 		time : 1200,
@@ -140,17 +153,26 @@ var residentList = [ {
 	} ],
 	contact : [ {
 		name : "Harry Armstrong",
-		relation : "Father",
+		relation : "Son",
 		contact : 98765425
 	}, {
 		name : "Angelababy",
-		relation : "Mother",
+		relation : "Wife",
 		contact : 56489565
 	} ]
 }, {
 	name : "Derrick",
 	bed : 104,
 	image : '//lorempixel.com/150/150/people/4/',
+	news : [ {
+		date : "11 Oct, Sun, 0200hrs",
+		icon : "./img/NewsFeedIcon.png",
+		description : "Derrick crept out to watch TV during wee hours"
+	}, {
+		date : "10 Oct, Sat, 1200hrs",
+		icon : "./img/MedicineIcon.png",
+		description : "Derrick's medication is updated"
+	} ],
 	pastAppt : [ {
 		date : "11 Sep",
 		time : 1200,
@@ -171,17 +193,26 @@ var residentList = [ {
 	} ],
 	contact : [ {
 		name : "Harry Armstrong",
-		relation : "Father",
+		relation : "Son",
 		contact : 98765425
 	}, {
 		name : "Angelababy",
-		relation : "Mother",
+		relation : "Wife",
 		contact : 56489565
 	} ]
 }, {
 	name : "Alex",
 	bed : 105,
 	image : '//lorempixel.com/150/150/people/5/',
+	news : [ {
+		date : "11 Oct, Sun, 1120hrs",
+		icon : "./img/NewsFeedIcon.png",
+		description : "Alex wet his pants in the morning"
+	}, {
+		date : "10 Oct, Sat, 2200hrs",
+		icon : "./img/NewsFeedIcon.png",
+		description : "Alex resisted taking his bladder pills"
+	} ],
 	pastAppt : [ {
 		date : "11 Sep",
 		time : 1200,
@@ -202,11 +233,11 @@ var residentList = [ {
 	} ],
 	contact : [ {
 		name : "Harry Armstrong",
-		relation : "Father",
+		relation : "Son",
 		contact : 98765425
 	}, {
 		name : "Angelababy",
-		relation : "Mother",
+		relation : "Wife",
 		contact : 56489565
 	} ]
 } ];
--- WebContent/js/resident.js
+++ WebContent/js/resident.js
@@ -10,51 +10,54 @@
 var residentString = localStorage.getItem("resident");
 var resident = JSON.parse(residentString);
 console.log(resident);
-var image = '<img class="profile-thumbnail" src="'+resident.image+'"/>';
+var image = '<img class="profile-thumbnail" src="' + resident.image + '"/>';
 $("#residentPic").html(image);
 $("#residentName").html(resident.name);
-$("#bed").html('bed number '+ resident.bed);
+$("#bed").html('bed number ' + resident.bed);
 var newsList = resident.news;
-//console.log(newsList);
+// console.log(newsList);
 
-//function displayFeeds(){
-	var newshtml = '<li data-role="list-divider">Resident Newsfeed</li>';
-//	console.log(newsList);
-	for ( var n in newsList) {
-		var feed = newsList[n];
-		var residentBed = feed.bed;
-		newshtml += '\
+// function displayFeeds(){
+var newshtml = '<li data-role="list-divider">Resident Newsfeed</li>';
+// console.log(newsList);
+for ( var n in newsList) {
+	var feed = newsList[n];
+	var residentBed = feed.bed;
+	newshtml += '\
 		<li>\
-			<img src="'+feed.icon+'" class="ui-thumbnail ui-thumbnail-circular" />\
-			<h3>' + feed.date + '</h2>\
-			<p>' + feed.description + '</p>\
+			<img src="' + feed.icon
+			+ '" class="ui-thumbnail ui-thumbnail-circular" />\
+			<h3>'
+			+ feed.date + '</h2>\
+			<p>' + feed.description
+			+ '</p>\
 		</li>';
-	}
-	$('#news').html(newshtml);
-//}
+}
+$('#news').html(newshtml);
+// }
 
-//function postFeed(){
-//	var feed = $("#feedText").val();
-//	document.getElementById("createFeed").reset();
-//	console.log(feed);
-//	var newFeed = {
-//			date : "Just now",
-//			icon : "./img/NewsFeedIcon.png",
-//			description : feed};
-//	newsList.push(newFeed);
+// function postFeed(){
+// var feed = $("#feedText").val();
+// document.getElementById("createFeed").reset();
+// console.log(feed);
+// var newFeed = {
+// date : "Just now",
+// icon : "./img/NewsFeedIcon.png",
+// description : feed};
+// newsList.push(newFeed);
 //	
-//	var newshtml = '<li data-role="list-divider">Resident Newsfeed</li>';
-//	console.log(newsList);
-//	for ( var n in newsList) {
-//		var feed = newsList[n];
-//		var residentBed = feed.bed;
-//		newshtml += '\
-//		<li>\
-//			<img src="'+feed.icon+'" class="ui-thumbnail ui-thumbnail-circular" />\
-//			<h3>' + feed.date + '</h2>\
-//			<p>' + feed.description + '</p>\
-//		</li>';
-//	}
-//	$('#news').html(newshtml);
-////	displayFeeds();
-//}
+// var newshtml = '<li data-role="list-divider">Resident Newsfeed</li>';
+// console.log(newsList);
+// for ( var n in newsList) {
+// var feed = newsList[n];
+// var residentBed = feed.bed;
+// newshtml += '\
+// <li>\
+// <img src="'+feed.icon+'" class="ui-thumbnail ui-thumbnail-circular" />\
+// <h3>' + feed.date + '</h2>\
+// <p>' + feed.description + '</p>\
+// </li>';
+// }
+// $('#news').html(newshtml);
+// // displayFeeds();
+// }
