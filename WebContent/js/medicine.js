cc9609bacf100fbdc0983a7b46a349ef1b36109c
Ray <rui.song.2013@sis.smu.edu.sg>
2015-10-17 01:28:50 +0800

    fix login page

---
+++ WebContent/js/medicine.js
@@ -0,0 +1,67 @@
+$(document).ready(function() {
+	$("#editableForm").hide();
+	
+});
+
+$("#editBtn_medicine").click(function(event) {
+	event.preventDefault();
+	
+	$('#displayForm').hide();
+//	console.log($('#dosage').html());
+//	console.log('400mg');
+////	$('#dosageEdit').val($('#dosage').html());
+//	document.getElementById('dosageEdit').value = $('#dosage').html();
+////	document.getElementById('frequencyEdit').value = $('#frequency').html();
+//	$("#frequencyEdit").val($('#frequency').html());
+//	document.getElementById('startDateEdit').value = $('#startDate').html();
+//	$("#startDateEdit").val($('#startDate').html());
+//	document.getElementById('sideEffectEdit').value = $('#sideEffect').html();
+	$("#editableForm").fadeIn();
+
+});
+
+$("#saveBtn_medicine").click(function(event) {
+	event.preventDefault();
+	
+	$('#dosageEdit').val();
+	$("#frequencyEdit").val();
+	$("#startDateEdit").val();
+	$("#sideEffectEdit").val();
+	
+	
+	var username = $("#regUsername").val();
+	var password = $("#regPassword").val();
+	var password2 = $("#confirmPassword").val();
+	
+	localStorage.setItem('username', username);
+	localStorage.setItem('password', password);
+	
+	$("#editableForm").hide();
+	$('#displayForm').fadeIn();
+});
+
+
+var residentString = localStorage.getItem("resident");
+var resident = JSON.parse(residentString);
+console.log(resident);
+var image = '<img class="profile-thumbnail" src="'+resident.image+'"/>';
+$("#residentPic").html(image);
+$("#residentName").html(resident.name);
+$("#bed").html('bed number '+ resident.bed);
+var newsList = resident.news;
+//console.log(newsList);
+
+//function displayFeeds(){
+	var medicineshtml = '<li data-role="list-divider">Resident Newsfeed</li>';
+//	console.log(newsList);
+	for ( var n in newsList) {
+		var feed = newsList[n];
+		var residentBed = feed.bed;
+		medicineshtml += '\
+		<li>\
+			<img src="'+feed.icon+'" class="ui-thumbnail ui-thumbnail-circular" />\
+			<h3>' + feed.date + '</h2>\
+			<p>' + feed.description + '</p>\
+		</li>';
+	}
+	$('#medicines').html(medicineshtml);
--- WebContent/js/resident.js
+++ WebContent/js/resident.js
@@ -1,3 +1,12 @@
+//$(document).ready(function() {
+//	displayFeeds();
+//
+//});
+
+//$(document).on("pageinit", "#resident-main", function(event) {
+//	
+//});
+
 var residentString = localStorage.getItem("resident");
 var resident = JSON.parse(residentString);
 console.log(resident);
@@ -5,17 +14,47 @@ var image = '<img class="profile-thumbnail" src="'+resident.image+'"/>';
 $("#residentPic").html(image);
 $("#residentName").html(resident.name);
 $("#bed").html('bed number '+ resident.bed);
+var newsList = resident.news;
+//console.log(newsList);
 
-var newshtml = '<li data-role="list-divider">Resident Newsfeed</li>';
-var newsList = resident.news
-for ( var n in newsList) {
-	var feed = newsList[n];
-	var residentBed = feed.bed;
-	newshtml += '\
-	<li>\
-		<img src="'+feed.icon+'" class="ui-thumbnail ui-thumbnail-circular" />\
-		<h3>' + feed.date + '</h2>\
-		<p>' + feed.description + '</p>\
-	</li>';
-}
-$('#news').html(newshtml);
+//function displayFeeds(){
+	var newshtml = '<li data-role="list-divider">Resident Newsfeed</li>';
+//	console.log(newsList);
+	for ( var n in newsList) {
+		var feed = newsList[n];
+		var residentBed = feed.bed;
+		newshtml += '\
+		<li>\
+			<img src="'+feed.icon+'" class="ui-thumbnail ui-thumbnail-circular" />\
+			<h3>' + feed.date + '</h2>\
+			<p>' + feed.description + '</p>\
+		</li>';
+	}
+	$('#news').html(newshtml);
+//}
+
+//function postFeed(){
+//	var feed = $("#feedText").val();
+//	document.getElementById("createFeed").reset();
+//	console.log(feed);
+//	var newFeed = {
+//			date : "Just now",
+//			icon : "./img/NewsFeedIcon.png",
+//			description : feed};
+//	newsList.push(newFeed);
+//	
+//	var newshtml = '<li data-role="list-divider">Resident Newsfeed</li>';
+//	console.log(newsList);
+//	for ( var n in newsList) {
+//		var feed = newsList[n];
+//		var residentBed = feed.bed;
+//		newshtml += '\
+//		<li>\
+//			<img src="'+feed.icon+'" class="ui-thumbnail ui-thumbnail-circular" />\
+//			<h3>' + feed.date + '</h2>\
+//			<p>' + feed.description + '</p>\
+//		</li>';
+//	}
+//	$('#news').html(newshtml);
+////	displayFeeds();
+//}
